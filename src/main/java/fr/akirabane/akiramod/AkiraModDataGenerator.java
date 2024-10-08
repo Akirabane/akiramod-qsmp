ba80f69ad58538c6b5112036a6f0a5786d0d6a40
Akirabane <joshuabenon988@yahoo.fr>
2024-10-08 19:57:51 +0200

    Initial commit
    premier commit

---
+++ src/main/java/fr/akirabane/akiramod/AkiraModDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.akirabane.akiramod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AkiraModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
